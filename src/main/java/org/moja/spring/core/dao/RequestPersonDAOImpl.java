519243f558d220972f7be832b63d9676f8aa98c8
Moja <azimi_moja@yahoo.com>
2021-01-25 17:02:25 +0330

    add jdbcTemplate

--- src/main/java/org/moja/spring/core/dao/RequestPersonDAOImpl.java
+++ src/main/java/org/moja/spring/core/dao/RequestPersonDAOImpl.java
@@ -2,6 +2,7 @@ package org.moja.spring.core.dao;
 
 import org.moja.spring.core.dao.impl.RequestPersonDAO;
 import org.moja.spring.core.entity.RequestPerson;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -11,36 +12,50 @@ import java.util.List;
 
 public class RequestPersonDAOImpl implements RequestPersonDAO {
     private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
+
+    /*
+     *JDBC connection example
+     */
+//    @Override
+//    public void saveRequestPerson(RequestPerson requestPerson) {
+//        Connection connection = null;
+//        PreparedStatement ps = null;
+//        try {
+//            connection = dataSource.getConnection();
+//            String query = "insert into request_person(first_name, last_name) VALUES(?, ?)";
+//            ps = connection.prepareStatement(query);
+//            ps.setString(1, requestPerson.getFirstName());
+//            ps.setString(2, requestPerson.getLastName());
+//            int result = ps.executeUpdate();
+//            if(result > 0){
+//                System.out.println("requestPerson is added!!!");
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                ps.close();
+//                if (connection != null) {
+//                    connection.close();
+//                }
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 
     @Override
     public void saveRequestPerson(RequestPerson requestPerson) {
-        Connection connection = null;
-        PreparedStatement ps = null;
-        try {
-            connection = dataSource.getConnection();
-            String query = "insert into request_person(first_name, last_name) VALUES(?, ?)";
-            ps = connection.prepareStatement(query);
-            ps.setString(1, requestPerson.getFirstName());
-            ps.setString(2, requestPerson.getLastName());
-            int result = ps.executeUpdate();
-            if(result > 0){
-                System.out.println("requestPerson is added!!!");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                ps.close();
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        String query = "insert into request_person(first_name, last_name) VALUES(?, ?)";
+        int result = jdbcTemplate.update(query, new Object[]{requestPerson.getFirstName(), requestPerson.getLastName()});
+        if (result > 0) {
+            System.out.println("requestPerson is added!!!");
         }
     }
 
